c57a866ec67c234c6b79040757a792d8a0c129de
nipun-dezoysa <nipunavishka123@gmail.com>
2024-12-03 23:57:26 +0530

    feat: add image entity class

---
+++ src/main/java/com/naprojects/shoppingcart/model/Image.java
@@ -0,0 +1,28 @@
+package com.naprojects.shoppingcart.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Blob;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileName;
+    private String fileType;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
