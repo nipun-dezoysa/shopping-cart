925e77cc4a48b9a00a5d1a026021155e6173bf85
nipun-dezoysa <nipunavishka123@gmail.com>
2024-12-05 19:31:45 +0530

    feat: product service and repository

--- src/main/java/com/naprojects/shoppingcart/repository/ProductRepository.java
+++ src/main/java/com/naprojects/shoppingcart/repository/ProductRepository.java
@@ -3,5 +3,18 @@ package com.naprojects.shoppingcart.repository;
 import com.naprojects.shoppingcart.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByCategoryName(String category);
+
+    List<Product> findByBrand(String brand);
+
+    List<Product> findByCategoryNameAndBrand(String category, String brand);
+
+    List<Product> findByName(String name);
+
+    List<Product> findByBrandAndName(String brand, String name);
+
+    Long countByBrandAndName(String brand, String name);
 }
