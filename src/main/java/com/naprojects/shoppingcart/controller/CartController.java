b567bc1dc210994f30bcc13d4aa145eca4def16b
nipun-dezoysa <nipunavishka123@gmail.com>
2024-12-08 12:22:15 +0530

    feat: add Cart Controller

---
+++ src/main/java/com/naprojects/shoppingcart/controller/CartController.java
@@ -0,0 +1,49 @@
+package com.naprojects.shoppingcart.controller;
+
+import com.naprojects.shoppingcart.exceptions.ResourceNotFoundException;
+import com.naprojects.shoppingcart.model.Cart;
+import com.naprojects.shoppingcart.response.ApiResponse;
+import com.naprojects.shoppingcart.service.cart.ICartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/carts")
+public class CartController {
+    private final ICartService cartService;
+
+    @GetMapping("/{cartId}/my-cart")
+    public ResponseEntity<ApiResponse> getCart(@PathVariable Long cartId) {
+        try {
+            Cart cart = cartService.getCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Success!", cart));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/{cartId}/clear")
+    public ResponseEntity<ApiResponse> clearCart( @PathVariable Long cartId) {
+        try {
+            cartService.clearCart(cartId);
+            return ResponseEntity.ok(new ApiResponse("Cleared!", null));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/{cartId}/cart/total-price")
+    public ResponseEntity<ApiResponse> getTotalAmount( @PathVariable Long cartId) {
+        try {
+            BigDecimal totalPrice = cartService.getTotalPrice(cartId);
+            return ResponseEntity.ok(new ApiResponse("Total Price", totalPrice));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
